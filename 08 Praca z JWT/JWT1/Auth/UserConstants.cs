38bad41cd77ae3bc5a406c72c72f5859b50ae999
mradon1337 <m.radon1337@gmail.com>
2026-06-01 22:46:51 +0200

    JWT1

---
+++ 08 Praca z JWT/JWT1/Auth/UserConstants.cs
@@ -0,0 +1,13 @@
+using BlogCMS.Models;
+
+namespace BlogCMS.Auth
+{
+    
+    public class UserConstants
+    {
+        public static List<LoginModel> Users = new()
+        {
+            new LoginModel { Username = "Grzegorz", Password = "TajneHaslo_1234", Role = "Admin" }
+        };
+    }
+}
